1819851b908e615a4421db292200a5f6c5c1a981
Jonathan Wan <jnthnwn@gmail.com>
2016-05-19 23:00:09 -0600

    Add display socket to send messages to all subscribers

--- chatroom.py
+++ chatroom.py
@@ -5,12 +5,15 @@ def main():
   ctx = zmq.Context()
   client_sock = ctx.socket(zmq.REP)
   client_sock.bind('tcp://*:8888')
+  display_sock = ctx.socket(zmq.PUB)
+  display_sock.bind('tcp://*:8889')
   
   while True:
     parts = client_sock.recv_multipart()
     identity, msg = [s.decode() for s in parts]
     print('{}: {}'.format(identity, msg))
     client_sock.send(b'\x00')
+    display_sock.send_multipart(parts)
 
 
 if '__main__' == __name__:
