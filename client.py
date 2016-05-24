0a37ade72a768d81ddd7512288c5d6b3a93b4883
Jonathan Wan <jnthnwn@gmail.com>
2016-05-23 22:18:29 -0600

    Add basic logic for failure to connect to server

--- client.py
+++ client.py
@@ -7,13 +7,26 @@ def main():
   ctx = zmq.Context()
   chatroom_sock = ctx.socket(zmq.REQ)
   chatroom_sock.connect('tcp://localhost:8888')
+  poller = zmq.Poller()
+  poller.register(chatroom_sock, zmq.POLLIN)
+
   while True:
     msg = input('> ')
     parts = [identity, msg]
     chatroom_sock.send_multipart([bytes(part, 'utf-8') for part in parts])
-    print('sent msg: {}'.format(parts))
-    reply = chatroom_sock.recv()
-    print('received reply: {}'.format(str(reply)))
+
+    # wait up to 1000 ms for a reply
+    events = dict(poller.poll(1000))
+    if events.get(chatroom_sock) == zmq.POLLIN:
+      reply = chatroom_sock.recv()
+    else:
+      print('failed to send message - response timed out')
+      chatroom_sock.setsockopt(zmq.LINGER, 0)
+      chatroom_sock.close()
+      poller.unregister(chatroom_sock)
+      chatroom_sock = ctx.socket(zmq.REQ)
+      chatroom_sock.connect('tcp://localhost:8888')
+      poller.register(chatroom_sock, zmq.POLLIN)
 
 
 if '__main__' == __name__:
