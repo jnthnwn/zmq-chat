7d9413f781c55754ac7e6003051ac0a45e70de7d
Jonathan Wan <jnthnwn@gmail.com>
2016-07-03 15:31:01 -0600

    Refactor server to a class

--- server.py
+++ server.py
@@ -1,19 +1,69 @@
+import argparse
 import zmq
 
 
-def main():
-  ctx = zmq.Context()
-  client_sock = ctx.socket(zmq.REP)
-  client_sock.bind('tcp://*:8888')
-  display_sock = ctx.socket(zmq.PUB)
-  display_sock.bind('tcp://*:8889')
-  
-  while True:
-    parts = client_sock.recv_multipart()
-    identity, msg = [s.decode() for s in parts]
-    client_sock.send(b'\x00')
-    display_sock.send_multipart(parts)
+class ZMQChatServer(object):
+
+    def __init__(self, chat_interface, chat_port, display_interface, display_port):
+        self.chat_interface = chat_interface
+        self.chat_port = chat_port
+        self.display_interface = display_interface
+        self.display_port = display_port
+        self.context = zmq.Context()
+        self.chat_sock = None
+        self.display_sock = None
+
+    def bind_ports(self):
+        self.chat_sock = self.context.socket(zmq.REP)
+        chat_bind_string = 'tcp://{}:{}'.format(
+            self.chat_interface, self.chat_port)
+        self.chat_sock.bind(chat_bind_string)
+
+        self.display_sock = self.context.socket(zmq.PUB)
+        display_bind_string = 'tcp://{}:{}'.format(
+            self.display_interface, self.display_port)
+        self.display_sock.bind(display_bind_string)
+
+    def get_message_with_identity(self):
+        parts = self.chat_sock.recv_multipart()
+        print(parts)
+        identity, message = [s.decode() for s in parts]
+        return [identity, message]
+
+    def update_displays(self, identity, message):
+        parts = [identity, message]
+        parts = [bytes(s, 'utf-8') for s in parts]
+        self.chat_sock.send(b'\x00')
+        self.display_sock.send_multipart(parts)
+
+    def start_main_loop(self):
+        self.bind_ports()
+        while True:
+            identity, message = self.get_message_with_identity()
+            self.update_displays(identity, message)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Run the chat server')
+
+    parser.add_argument('chat_interface',
+                        type=str,
+                        help='name of interface for chat messages')
+    parser.add_argument('chat_port',
+                        type=str,
+                        help='port to expose for chat messages')
+    parser.add_argument('display_interface',
+                        type=str,
+                        help='name of interface for display messages')
+    parser.add_argument('display_port',
+                        type=str,
+                        help='port to expose for display messages')
+
+    return parser.parse_args()
 
 
 if '__main__' == __name__:
-  main()
+    args = parse_args()
+    server = ZMQChatServer(args.chat_interface, args.chat_port,
+                           args.display_interface, args.display_port)
+    server.start_main_loop()
