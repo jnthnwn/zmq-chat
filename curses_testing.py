ed0ff1eda17cefce014869f9a003973fed2e8f8c
Jonathan Wan <jnthnwn@gmail.com>
2016-07-03 22:59:18 -0600

    Add a bit of commenting for curses

--- curses_testing.py
+++ curses_testing.py
@@ -3,12 +3,19 @@ from curses import wrapper
 
 
 def main(stdscr):
-    stdscr.clear()
-
     for i in range(10):
+        # start with a blank screen
+        stdscr.clear()
+        # print the counter to the screen
         stdscr.addstr(str(i))
+        # update the screen contents
         stdscr.refresh()
+        # wait for user to press key
         stdscr.getkey()
 
-
+'''
+Puts a wrapper around a callable that will create a window object,
+turn off key-to-screen echoing, make applications react to keys instantly i.e. without waiting for a
+return key signal, and handle special keys more nicely.
+'''
 wrapper(main)
