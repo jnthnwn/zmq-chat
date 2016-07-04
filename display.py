703df847fcb863563a849dc22ceb0bf21c5908a0
Jonathan Wan <jnthnwn@gmail.com>
2016-07-03 22:15:57 -0600

    Refactor display to be classy

--- display.py
+++ display.py
@@ -1,31 +1,48 @@
+import argparse
 import sys
 import zmq
 
 
-def main():
-  args = parse_args()
-  
-  hostname = args.hostname
-  port = args.port
-  ctx = zmq.Context()
-  chatroom_sock = ctx.socket(zmq.SUB)
-  chatroom_sock.setsockopt_string(zmq.SUBSCRIBE, '')
-  chatroom_sock.connect('tcp://{}:{}'.format(hostname, port))
-  while True:
-    reply = chatroom_sock.recv_multipart()
-    user, msg = [s.decode() for s in reply]
-    print('{}: {}'.format(user, msg))
+class ZMQChatDisplay(object):
+
+    def __init__(self, server_host, server_port):
+        self.server_host = server_host
+        self.server_port = server_port
+        self.context = zmq.Context()
+        self.display_sock = None
+
+    def connect_to_server(self):
+        self.display_sock = self.context.socket(zmq.SUB)
+        self.display_sock.setsockopt_string(zmq.SUBSCRIBE, '')
+        connect_string = 'tcp://{}:{}'.format(
+            self.server_host, self.server_port)
+        self.display_sock.connect(connect_string)
+
+    def get_update(self):
+        reply = self.display_sock.recv_multipart()
+        user, message = [s.decode() for s in reply]
+        print('{}: {}'.format(user, message))
+
+    def start_main_loop(self):
+        self.connect_to_server()
+        while True:
+            self.get_update()
 
 
 def parse_args():
-  parser = argparse.ArgumentParser(description='Run the chat display')
-  parser.add_argument('hostname',
-          type=str,
-          help='hostname of the chat server')
-  parser.add_argument('port',
-          type=str,
-          help='port used for the chat server display')
+    parser = argparse.ArgumentParser(description='Run the chat display')
+
+    parser.add_argument('hostname',
+                        type=str,
+                        help='hostname of the chat server')
+    parser.add_argument('port',
+                        type=str,
+                        help='port used for the chat server display')
+
+    return parser.parse_args()
 
 
 if '__main__' == __name__:
-  main()
+    args = parse_args()
+    display = ZMQChatDisplay(args.hostname, args.port)
+    display.start_main_loop()
